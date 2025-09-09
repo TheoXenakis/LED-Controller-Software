6628608b7943c91e9dab8966752b5a43880810c2
TheoXenakis <theodore.xen7@gmail.com>
2025-09-09 13:42:53 -0700

    created basic readme

---
+++ led-controller.py
@@ -0,0 +1,12 @@
+"****************************************************************************"
+"*    CREATED BY THEO XENAKIS - Free to use for Prizmatix LED-Ctrl users    *"                                                                *"
+"****************************************************************************"
+
+"+---------------------------- led-controller.py ---------------------------+"
+
+"****************************************************************************"
+"* Getting Started:
+* 1) Install Python 3.6 or higher from https://www.python.org/downloads/
+* 2) Install the 'pyserial' library by running 'pip install pyserial' in your command line
+* 3) Connect your Prizmatix LED Controller to your computer via USB
+* 4) Open Device Manager (Windows) or System Information (Mac) to find the COM port
